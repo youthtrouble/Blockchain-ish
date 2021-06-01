b38fb88e650675f468f2652edc96fcdbe7643c79
Youthtrouble <harjhibolar@gmail.com>
2021-06-01 14:07:15 +0100

    correctly recieves and processes responses from other connections

--- main.go
+++ main.go
@@ -158,6 +158,33 @@ func spunNewRouter() http.Handler {
 
 func handleotherConn(conn net.Conn) {
 	defer conn.Close()
+
+	io.WriteString(conn, "Enter a new BPM:")
+
+	scanner := bufio.NewScanner(conn)
+
+	// take in BPM from stdin and add it to blockchain after conducting necessary validation
+	go func() {
+		for scanner.Scan() {
+			bpm, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Printf("%v not a number: %v", scanner.Text(), err)
+				continue
+			}
+			newBlock, err := generateBlock(Blockchainish[len(Blockchainish)-1], bpm)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if isBlockValid(newBlock, Blockchainish[len(Blockchainish)-1]) {
+				newBlockchain := append(Blockchainish, newBlock)
+				replaceChain(newBlockchain)
+			}
+
+			bcServer <- Blockchainish
+			io.WriteString(conn, "\nEnter a new BPM:")
+		}
+	}()
 }
 
 
@@ -190,33 +217,6 @@ func main() {
 		}
 		go handleotherConn(conn)
 	}
-
-	io.WriteString(conn, "Enter a new BPM:")
-
-	scanner := bufio.NewScanner(conn)
-
-	// take in BPM from stdin and add it to blockchain after conducting necessary validation
-	go func() {
-		for scanner.Scan() {
-			bpm, err := strconv.Atoi(scanner.Text())
-			if err != nil {
-				log.Printf("%v not a number: %v", scanner.Text(), err)
-				continue
-			}
-			newBlock, err := generateBlock(Blockchainish[len(Blockchainish)-1], bpm)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			if isBlockValid(newBlock, Blockchainish[len(Blockchainish)-1]) {
-				newBlockchain := append(Blockchainish, newBlock)
-				replaceChain(newBlockchain)
-			}
-
-			bcServer <- Blockchainish
-			io.WriteString(conn, "\nEnter a new BPM:")
-		}
-	}()
 
 }
 
